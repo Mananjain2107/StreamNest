4ca000532ad99a367eabf7370b302544bebcbaae
Manan Jain <writetomananjain21@gmail.com>
2025-04-07 12:22:50 +0530

    setup middleware structure

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import dotenv from 'dotenv';
 import connectDB from './db/db.js';
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000),()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    }
+})
+.catch((err)=>{
+    console.log("Connection Failed",err);
+})
